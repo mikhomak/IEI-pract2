9bc48efca1030b7f208d2909906b7409d58219d3
Misha <homakhaker@gmail.com>
2019-11-09 20:37:54 +0100

    Added a phone model

---
+++ PhoneSearch/src/persistence/models/Phone.java
@@ -0,0 +1,31 @@
+package persistence.models;
+
+public class Phone {
+    private String name;
+    private String price;
+    private String brand;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
